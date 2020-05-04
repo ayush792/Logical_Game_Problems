4696be17f4423bf1eb786872ad0f75482b2648b1
Ayush <vayush198@gmail.com>
2020-05-04 14:20:40 +0530

    Add:Added UC-2, to compute a+b*c

--- Arithmatic_Sorting.sh
+++ Arithmatic_Sorting.sh
@@ -4,3 +4,6 @@ read -p "Enter the first number -: " a
 read -p "Enter the second number -: " b
 read -p "Enter the third number -: " c
 
+compute_usecase2=$(($a + $b * $c));
+
+echo $compute_usecase2
