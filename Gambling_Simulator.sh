80b8def32585f847514f0cbe91eeb3c82e42b840
Ayush <vayush198@gmail.com>
2020-05-05 00:12:04 +0530

    Add:Added UC-3, Gambler won or lost 50% stake

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -6,6 +6,16 @@ bet=$(( RANDOM % 2 ))
 if [ $bet = 1 ]
 then
 	echo win
+if [[ $stake > 50 || $stake <150 ]]
+then
+	stake=$(($stake + 1))
+else
+	stake=$(($stake -1))
+fi
+
 else
 	echo loss
 fi
+
+echo $stake
+
