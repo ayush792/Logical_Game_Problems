111bd282e6240dcf692944fc77d2b18a23e076ee
Ayush <vayush198@gmail.com>
2020-05-04 16:08:01 +0530

    Add:Added UC-7, Read the values from Dictionary into Array

--- Arithmatic_Sorting.sh
+++ Arithmatic_Sorting.sh
@@ -4,21 +4,25 @@ read -p "Enter the first number -: " a
 read -p "Enter the second number -: " b
 read -p "Enter the third number -: " c
 
-compute_usecase2=$(($a + $b * $c));
-compute_usecase3=$(($a * $b + $c));
-compute_usecase4=$(($c + $a / $b));
-compute_usecase5=$(($a % $b + $c));
-
-echo $compute_usecase2
-echo $compute_usecase3
-echo $compute_usecase4
-echo $compute_usecase5
-
 declare -A UseCase
-UseCase[2]=$(($a + $b * $c))
-UseCase[3]=$(($a * $b + $c))
-UseCase[4]=$(($c + $a / $b))
-UseCase[5]=$(($a % $b + $c))
+compute_useCase2=$(($a + $b * $c));
+compute_useCase3=$(($a * $b + $c));
+compute_useCase4=$(($c + $a / $b));
+compute_useCase5=$(($a % $b + $c));
 
-echo "Usecases are" ${UseCase[@]}
+UseCase[2]=$compute_useCase2
+UseCase[3]=$compute_useCase3
+UseCase[4]=$compute_useCase4
+UseCase[5]=$compute_useCase5
 
+echo "Array Output is "
+count=0;
+UseCase[((count++))]=$compute_useCase2
+UseCase[((count++))]=$compute_useCase3
+UseCase[((count++))]=$compute_useCase4
+UseCase[((count++))]=$compute_useCase5
+echo ${UseCase[@]}
+
+echo "dictionary Output is"
+echo ${UseCase[@]}
+echo ${!UseCase[@]}
